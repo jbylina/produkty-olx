c61bcf28b8c45d33417b0a20fa276d903e47d576
Jacek Bylina <jacek.bylina@gmail.com>
2016-10-27 22:44:48 +0200

    Create date column in output csv

--- olx_parser.py
+++ olx_parser.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
+from time import gmtime, strftime
 import requests
 import csv
 
 
 def save_to_csv(array):
-    with open('olxWyniki.csv', 'w') as file:
+    with open('olxWyniki.csv', 'a') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
         for item in array:
             writer.writerow(item)
@@ -12,16 +13,16 @@ def save_to_csv(array):
 
 results = []
 
-for i in range(1, 2):
+for i in range(500, 1):
     request = requests.get("http://www.olx.pl/uslugi-firmy/piaseczno/?page=" + str(i))
-    soup = BeautifulSoup(request.text, "lxml")
+    soup = BeautifulSoup(request.text, "html.parser")
 
     for link in soup.findAll('a', {'class': 'marginright5'}):
         url = link['href']
 
         # get inside link from main page
         request = requests.get(url)
-        soup2 = BeautifulSoup(request.text, "lxml")
+        soup2 = BeautifulSoup(request.text, "html.parser")
 
         # find bottom bar div
         bottomBar = soup2.find('div', {'id': 'offerbottombar'})
@@ -30,7 +31,8 @@ for i in range(1, 2):
         bottomBarChildren = bottomBar.findAll('div', {'class': 'pdingtop10'})
         offer_count = bottomBarChildren[1].text.strip().split('Wyświetleń:', 1)[1]
 
-        # for div in bottomBarChildren:
-        results.append([url, int(offer_count)])
+        id = soup2.find('span', {'class': 'rel inlblk'}).text
+
+        results.append([int(id), url, strftime("%Y-%m-%d %H:%M:%S", gmtime()), int(offer_count)])
 
 save_to_csv(results)
