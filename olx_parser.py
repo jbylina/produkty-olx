0d3e393bb105759cefb573fe40fc2c136d37c649
mateuszzaborski <matzaborski@gmail.com>
2017-02-05 12:04:42 +0100

    test bokeh

--- olx_parser.py
+++ olx_parser.py
@@ -4,7 +4,7 @@ from time import gmtime, strftime
 import requests
 import csv
 import sys
-#import pandas as pd
+from bokeh.plotting import figure, output_file, show
 
 
 main_page_url = "https://www.olx.pl/uslugi-firmy/piaseczno/?page="
@@ -110,12 +110,25 @@ def check_views():
 
                 update_results(int(offer_id), url,strftime("%Y-%m-%d %H:%M:%S", gmtime()), int(offer_count))
 
-read_csv()
-if results:
-    print(results[1])
-shift_results()
-check_views()
-save_to_csv()
+#read_csv()
+#shift_results()
+#check_views()
+#save_to_csv()
 
-#for row in results:
-#    print(type(row[0]))
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+y2 = [5, 5, 3, 2, 4]
+
+# output to static HTML file
+output_file("index.html")
+
+# create a new plot with a title and axis labels
+p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+# add a line renderer with legend and line thickness
+p.line(x, y, legend="Temp.", line_width=2)
+p.line(x, y2, legend="Temp2", line_width=2)
+
+# show the results
+show(p)
